d03637d28a9e4875028f6972b9a32df9694eeee8
rttmoa <105489435+rttmoa@users.noreply.github.com>
2025-08-13 13:58:43 +0800

    fix: 🧩 del service file

--- service/objectql/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: sunhaolin@hotoa.com
- * @Date: 2023-06-08 17:42:46
- * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2023-06-08 17:45:11
- * @Description: 
- */
-declare var broker;
-  
---
+++ service/sql/Backup.sql
@@ -0,0 +1,4 @@
+
+
+
+mongodump -h 127.0.0.1 --port 27017 -d steedos_nbzy_v2 -o C:\
